ef25c0fffd3fae1911ef64aa2e3c4841b3c9791e
Russell Gordon <rgordon@rsgc.on.ca>
2015-11-13 15:52:59 -0500

    Initial Commit

---
+++ swift-serial/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-serial
+//
+//  Created by Russell Gordon on 2015-11-13.
+//  Copyright © 2015 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
