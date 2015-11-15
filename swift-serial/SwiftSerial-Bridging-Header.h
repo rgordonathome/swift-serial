c68b067474e56fe0af650b41d4194470dc71d3e4
Russell Gordon <rgordon@rsgc.on.ca>
2015-11-15 05:52:27 -0500

    Successfully reads serial input and prints to debug window

---
+++ swift-serial/SwiftSerial-Bridging-Header.h
@@ -0,0 +1,16 @@
+//
+//  SwiftSerial-Bridging-Header.h
+//  swift-serial
+//
+//  Created by Russell Gordon on 2015-11-15.
+//  Copyright © 2015 Russell Gordon. All rights reserved.
+//
+
+#ifndef SwiftSerial_Bridging_Header_h
+#define SwiftSerial_Bridging_Header_h
+
+
+#endif /* SwiftSerial_Bridging_Header_h */
+
+#import "ORSSerialPort.h"
+#import "ORSSerialPortManager.h"
